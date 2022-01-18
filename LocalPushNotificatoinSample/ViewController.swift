ad2b45dbb3455a1a250d2fcb4fd24a1ef67fbb38
ryoryoryoooo <P1211065@gmail.com>
2022-01-19 08:24:36 +0900

    ローカル通知の実装　ボタン押した２秒後にローカル通知

--- LocalPushNotificatoinSample/ViewController.swift
+++ LocalPushNotificatoinSample/ViewController.swift
@@ -15,5 +15,17 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func localPush(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "お知らせ"
+        content.body = "ボタンを押しました。"
+        content.sound = UNNotificationSound.default
+
+    
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(2), repeats: false)
+        let request = UNNotificationRequest(identifier: "immediately", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+
 }
 
